1fb691da4c4d222d18fc4e6a56874871a9658ff7
RoshanShrestha7532 <n01457532@humbermail.ca>
2022-11-13 15:29:16 -0500

    Date format updated in booking fragment

--- Smartparking/src/main/java/ca/friends/and/co/it/smartparking/BookingFragment.java
+++ Smartparking/src/main/java/ca/friends/and/co/it/smartparking/BookingFragment.java
@@ -3,16 +3,21 @@ package ca.friends.and.co.it.smartparking;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -78,6 +83,70 @@ public class BookingFragment extends Fragment {
         EditText contact = view.findViewById(R.id.contact_number);
         EditText date = view.findViewById(R.id.date);
         EditText duration = view.findViewById(R.id.duration);
+
+        date.addTextChangedListener(new TextWatcher() {
+
+            private String current = "";
+            private String ddmmyyyy = "DDMMYYYY";
+            private Calendar cal = Calendar.getInstance();
+
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)){
+                    String clean = s.toString().replaceAll("[^\\d.]", "");
+                    String cleanC = current.replaceAll("[^\\d.]", "");
+
+                    int cl = clean.length();
+                    int sel = cl;
+                    for (int i=2; i<=cl && i<6; i+=2){
+                        sel++;
+                    }
+
+                    if (clean.equals(cleanC)) sel--;
+
+                    if(clean.length()<8){
+                        clean =clean + ddmmyyyy.substring(clean.length());
+                    }else{
+                        int day = Integer.parseInt(clean.substring(0,2));
+                        int mon = Integer.parseInt(clean.substring(2,4));
+                        int year = Integer.parseInt(clean.substring(4,8));
+
+                        if (mon > 12) mon = 12;
+                        cal.set(Calendar.MONTH, mon-1);
+
+                        year = (year < 1900)?1900: (year>2100)?2100: year;
+                        cal.set(Calendar.YEAR, year);
+
+                        day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                        clean = String.format("%02d%02d%02d", day, mon, year);
+                    }
+
+                    clean = String.format("%s/%s/%s", clean.substring(0,2),
+                            clean.substring(2,4),
+                            clean.substring(4,8));
+
+                    sel = sel <0 ? 0 : sel;
+                    current = clean;
+                    date.setText(current);
+                    date.setSelection(sel < current.length() ? sel : current.length());
+                }
+
+            }
+
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
