af796e105a78cdb7a71e2ad3a91113e5ea79b2a5
RushiBhandari4259 <n01464259@humbermail.com>
2022-12-05 21:27:35 -0500

    Test cases 1 to type login email in espresso

---
+++ Smartparking/src/androidTest/java/ca/friends/and/co/it/smartparking/espressoTest.java
@@ -0,0 +1,34 @@
+package ca.friends.and.co.it.smartparking;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressKey;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.view.KeyEvent;
+import android.widget.EditText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class espressoTest {
+@Rule
+public ActivityScenarioRule<LoginActivity> activity = new ActivityScenarioRule<LoginActivity>(LoginActivity.class);
+
+@Test
+public void test_app_email(){
+    onView(withId(R.id.etLoginEmail)).perform(click());
+    onView(isAssignableFrom(EditText.class)).perform(typeText("bhandarirushi13@gmail.com"));
+
+}
+
+
+}
