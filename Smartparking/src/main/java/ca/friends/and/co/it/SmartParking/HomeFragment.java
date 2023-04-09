7f24c2d5ef92b801f933705aabb7e90521303f39
RameshNarayanGaire2430 <n01452430@humbermail.ca>
2023-04-09 13:42:20 -0400

    Gatearm added

--- Smartparking/src/main/java/ca/friends/and/co/it/SmartParking/HomeFragment.java
+++ Smartparking/src/main/java/ca/friends/and/co/it/SmartParking/HomeFragment.java
@@ -45,7 +45,7 @@ public class HomeFragment extends Fragment {
     ImageView imageView2;
     ImageView imageView3;
     ImageView imageView4;
-    Button gateArm;
+    ImageView gateArm;
 
     TextView bookHome;
     TextView feedbackHome;
@@ -110,7 +110,7 @@ public class HomeFragment extends Fragment {
         imageView2 = view.findViewById(R.id.support_imagebtn);
         imageView3 = view.findViewById(R.id.share_imagebtn);
         imageView4 = view.findViewById(R.id.feedback_imagebtn);
-        gateArm = view.findViewById(R.id.gateArm);
+        gateArm = view.findViewById(R.id.gatearm);
         bookHome = view.findViewById(R.id.book_home);
         feedbackHome = view.findViewById(R.id.feedback_home);
         supportHome = view.findViewById(R.id.support_home);
